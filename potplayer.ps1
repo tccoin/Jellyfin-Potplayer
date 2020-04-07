d0f013cdc3f26dd237c85e10328f29393d343b70
Link Wu <1774241372@qq.com>
2020-04-07 22:02:38 +0800

    use urldecode

--- potplayer.ps1
+++ potplayer.ps1
@@ -1,5 +1,6 @@
+Add-Type -Assembly System.Web
 $path=$args[0]
 $path=$path -replace "potplayer://" , ""
-$path=$path -replace "%20" , " "
+$path= [System.Web.HttpUtility]::UrlDecode($path)
 echo $path
 & "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
