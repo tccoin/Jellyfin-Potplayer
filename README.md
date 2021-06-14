51c887063273777297d5a27e7b6cc544272899be
Link Wu <1774241372@qq.com>
2021-06-15 01:03:34 +0800

    update installation method

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Jellyfin-Potplayer
- Play with Potplayer
+
+Tested with Jellyfin 10.5.2 and Windows 10. Newer version is not promised to work with this script. If you have any problem, please open an issue and I'll try my best to help you.
+
+## Installation
+
+1. Download this repository.
+2. Put `potplayer.ps1` somewhere else on your disk (DO NOT delete it even after you finish this tutorial). Edit the Potplayer path in `potplayer.ps1`. Notice that you should use double backslash for divider.
+3. Edit `potplayer.reg`. Change the path to where you put `potplayer.ps1`. Use double backslash also.
+4. Double click `potplayer.reg` and click yes.
+5. Install the extension called `Tampermonkey BETA` in your browser.
+6. Edit `userscript.js`.
+    1. Change `localhost:8096` to the host of your Jellyfin server.
+    2. This step is tricky... We need to make sure that the path to video showed in your Jellyfin webpage is corresponding to the actual place on your computer. So, if you are using this script on the same computer which runs the Jellyfin server, just skip to step 5. If not, please right click on blank area of "This computer" window and choose "add a network location" (or something like it, I use Chinese for the system so I don't know). Link the drive where you put all the videos on your Jellyfin server to a local drive. For example, after the setup, you should be able to open (for the server) `D:/folder1/video1.mp4` by (on your local computer) `Z:/folder1/video1.mp4`.
+    4.  Change line 16 in the script. Firstly, uncomment the line by deleting the leading double backslash. Then change `D:` to the drive on your server to the drive on your local computer. If you have some experience on Javascript, your can custom this path processing script by your own. Just make sure the path will be same to your local file path. For the example mentioned in step 2, the script should be like:
+    ```
+    path = path.replace('D:', 'Z:');
+    ```
+    5. Choose "Add a new script" in the `Tampermonkey` menu. Copy and paste the whole script. Make sure you enabled the script and when you open Jellyfin, the script is enabled.
+7. Test by click the play button! It should work fine now! Enjoy your movies!
+
+## How it works
+
+A user script modifies the play buttons in Jellyfin so that the web player will not be called. Instead, a link will be opened and it's a "url protocol" for Potplayer. The url protocol will firstly call a powershell script and then pass the video path to Potplayer. That's it!
+
--- userscript.js
+++ userscript.js
@@ -3,90 +3,91 @@
 // @version      0.1
 // @description  play video with Potplayer
 // @author       Tccoin
-// @match        http://10.0.0.17:8096/jellyfin/web/index.html
+// @match        http://localhost:8096/jellyfin/web/index.html
 // ==/UserScript==
 
-(function () {
-    'use strict';
-    let openPotplayer = (itemid) => {
-        let userid = ConnectionManager.getLastUsedServer().UserId;
-        ApiClient.getItem(userid, itemid).then(r => {
-            if (r.Path) {
-                let path = r.Path.replace(/\\/g, '/').replace('D:', 'Z:');
-                console.log(path);
-                window.open('potplayer://' + path)
-            } else {
-                ApiClient.getItems(userid, itemid).then(r => openPotplayer(r.Items[0].Id));
-            }
-        })
+(function() {
+  'use strict';
+  let openPotplayer = (itemid) => {
+    let userid = ConnectionManager.getLastUsedServer().UserId;
+    ApiClient.getItem(userid, itemid).then(r => {
+      if (r.Path) {
+        let path = r.Path.replace(/\\/g, '/');
+        //path = path.replace('D:', 'Z:');
+        console.log(path);
+        window.open('potplayer://' + path)
+      } else {
+        ApiClient.getItems(userid, itemid).then(r => openPotplayer(r.Items[0].Id));
+      }
+    })
+  };
+
+  let bindEvent = async() => {
+    let buttons = [];
+    let retry = 6 + 1;
+    while (buttons.length == 0 && retry > 0) {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      buttons = document.querySelectorAll('[data-mode=play],[data-mode=resume],[data-action=resume]');
+      retry -= 1;
+    }
+    for (let button of buttons) {
+      let nextElementSibling = button.nextElementSibling;
+      let parentElement = button.parentElement;
+      let outerHTML = button.outerHTML;
+      button.parentElement.removeChild(button);
+      let newButton = document.createElement('button');
+      if (nextElementSibling) {
+        parentElement.insertBefore(newButton, nextElementSibling);
+      } else {
+        parentElement.append(newButton);
+      }
+      newButton.outerHTML = outerHTML;
+    }
+    buttons = document.querySelectorAll('[data-mode=play],[data-mode=resume]');
+    for (let button of buttons) {
+      button.removeAttribute('data-mode');
+      button.addEventListener('click', e => {
+        e.stopPropagation();
+        let itemid = /id=(.*?)&serverId/.exec(window.location.hash)[1];
+        openPotplayer(itemid);
+      });
+    }
+    buttons = document.querySelectorAll('[data-action=resume]');
+    for (let button of buttons) {
+      button.removeAttribute('data-action');
+      button.addEventListener('click', e => {
+        e.stopPropagation();
+        let item = e.target;
+        while (!item.hasAttribute('data-id')) { item = item.parentNode }
+        let itemid = item.getAttribute('data-id');
+        openPotplayer(itemid);
+      });
+    }
+  };
+
+  let lazyload = () => {
+    let items = document.querySelectorAll('[data-src].lazy');
+    let y = document.scrollingElement.scrollTop;
+    let intersectinglist = [];
+    for (let item of items) {
+      let windowHeight = document.body.offsetHeight;
+      let itemTop = item.getBoundingClientRect().top;
+      let itemHeight = item.offsetHeight;
+      if (itemTop + itemHeight >= 0 && itemTop <= windowHeight) {
+        intersectinglist.push(item);
+      }
+    }
+    for (let item of intersectinglist) {
+      item.style.setProperty('background-image', `url("${item.getAttribute('data-src')}")`);
+      item.classList.remove('lazy');
+      item.removeAttribute('data-src');
     };
+  };
 
-    let bindEvent = async () => {
-        let buttons = [];
-        let retry = 6 + 1;
-        while (buttons.length == 0 && retry > 0) {
-            await new Promise(resolve => setTimeout(resolve, 500));
-            buttons = document.querySelectorAll('[data-mode=play],[data-mode=resume],[data-action=resume]');
-            retry -= 1;
-        }
-        for (let button of buttons) {
-            let nextElementSibling = button.nextElementSibling;
-            let parentElement = button.parentElement;
-            let outerHTML = button.outerHTML;
-            button.parentElement.removeChild(button);
-            let newButton = document.createElement('button');
-            if (nextElementSibling) {
-                parentElement.insertBefore(newButton, nextElementSibling);
-            } else {
-                parentElement.append(newButton);
-            }
-            newButton.outerHTML = outerHTML;
-        }
-        buttons = document.querySelectorAll('[data-mode=play],[data-mode=resume]');
-        for (let button of buttons) {
-            button.removeAttribute('data-mode');
-            button.addEventListener('click', e => {
-                e.stopPropagation();
-                let itemid = /id=(.*?)&serverId/.exec(window.location.hash)[1];
-                openPotplayer(itemid);
-            });
-        }
-        buttons = document.querySelectorAll('[data-action=resume]');
-        for (let button of buttons) {
-            button.removeAttribute('data-action');
-            button.addEventListener('click', e => {
-                e.stopPropagation();
-                let item = e.target;
-                while (!item.hasAttribute('data-id')) { item = item.parentNode }
-                let itemid = item.getAttribute('data-id');
-                openPotplayer(itemid);
-            });
-        }
-    };
+  window.addEventListener('scroll', lazyload);
 
-    let lazyload = () => {
-        let items = document.querySelectorAll('[data-src].lazy');
-        let y = document.scrollingElement.scrollTop;
-        let intersectinglist = [];
-        for (let item of items) {
-            let windowHeight = document.body.offsetHeight;
-            let itemTop = item.getBoundingClientRect().top;
-            let itemHeight = item.offsetHeight;
-            if (itemTop + itemHeight >= 0 && itemTop <= windowHeight) {
-                intersectinglist.push(item);
-            }
-        }
-        for (let item of intersectinglist) {
-            item.style.setProperty('background-image', `url("${item.getAttribute('data-src')}")`);
-            item.classList.remove('lazy');
-            item.removeAttribute('data-src');
-        };
-    };
-
-    window.addEventListener('scroll', lazyload);
-
-    window.addEventListener('viewshow', async () => {
-        bindEvent();
-        window.addEventListener('hashchange', bindEvent);
-    });
+  window.addEventListener('viewshow', async() => {
+    bindEvent();
+    window.addEventListener('hashchange', bindEvent);
+  });
 })();
